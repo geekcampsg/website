db6e8217b30cee6cab89794dbeff39c6f6b46bd5
Sheldon Cheng <sheldonscyd@gmail.com>
2019-09-28 23:41:13 +0800

    Add past events content

--- src/pages/past-events.js
+++ src/pages/past-events.js
@@ -1,11 +1,60 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Header from "../components/header"
 import { Schedule, ScheduleItem } from "../components/Schedule"
 
+import Event2018Plus from "../../data/archive/2018"
+import Event2017 from "../../data/archive/2017"
+
+const events = [
+  ["GeekcampSG 2018+", Event2018Plus],
+  ["GeekcampSG 2017", Event2017],
+]
+
+const SideLinks = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  font-size: 1.333333333rem;
+
+  li:hover {
+    color: var(--blue);
+    cursor: pointer;
+  }
+
+  li.is-active {
+    color: var(--green);
+  }
+`
+
+const EventTitle = styled.h3`
+  font-size: 2.222222222rem;
+  margin: 0.5rem 0 0;
+  border-top: 2px solid var(--yellow);
+  padding-top: 1rem;
+  @media screen and (min-width: 1280px) {
+    padding-top: 0;
+    border-top: none;
+  }
+`
+
+const EventDate = styled.div`
+  font-size: 1.777777778rem;
+  margin-bottom: 3rem;
+`
+
+const TalkVideo = styled.a`
+  color: var(--green);
+  font-size: 1.333333333rem;
+  font-weight: 400;
+`
+
 const PastEvents = () => {
+  const [event, setEvent] = useState(Event2018Plus)
+
   return (
     <Layout>
       <Header />
@@ -14,33 +63,39 @@ const PastEvents = () => {
           <div className="vessel cols">
             <div className="left">
               <h2>Past Events</h2>
+              <SideLinks>
+                {events.map(evt => (
+                  <li
+                    className={event === evt[1] ? "is-active" : null}
+                    onClick={() => setEvent(evt[1])}
+                  >
+                    {evt[0]}
+                  </li>
+                ))}
+              </SideLinks>
             </div>
             <div className="right">
+              <EventTitle>GeekcampSG {event.year}</EventTitle>
+              <EventDate>{event.date}</EventDate>
               <Schedule>
-                <ScheduleItem id="speaker-kendrick">
-                  <h3>
-                    Building Web-Based Audio Player for Obscure Audio Format
-                  </h3>
-                  <h4>Kenrick</h4>
-                  <p>
-                    BRSTM is an audio file format that are used on Nintendo Wii
-                    games and many others. This talk will cover the journey in
-                    building this player which decodes the BRSTM file entirely
-                    on browser and plays it back using Web Audio API.
-                  </p>
-                </ScheduleItem>
-                <ScheduleItem id="speaker-kendrick">
-                  <h3>
-                    Building Web-Based Audio Player for Obscure Audio Format
-                  </h3>
-                  <h4>Kenrick</h4>
-                  <p>
-                    BRSTM is an audio file format that are used on Nintendo Wii
-                    games and many others. This talk will cover the journey in
-                    building this player which decodes the BRSTM file entirely
-                    on browser and plays it back using Web Audio API.
-                  </p>
-                </ScheduleItem>
+                {event.talks.map(talk => (
+                  <ScheduleItem>
+                    <h3>
+                      {talk.title}{" "}
+                      {talk.video ? (
+                        <TalkVideo
+                          href={talk.video}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          Video
+                        </TalkVideo>
+                      ) : null}
+                    </h3>
+                    <h4>{talk.speaker}</h4>
+                    <p>{talk.summary}</p>
+                  </ScheduleItem>
+                ))}
               </Schedule>
             </div>
           </div>
