a13623b8c6405da714c879edfdf33a318091e7c9
Sheldon Cheng <sheldonscyd@gmail.com>
2019-09-29 22:34:06 +0800

    Update sponsors for responsive

--- src/sections/10.sponsors.js
+++ src/sections/10.sponsors.js
@@ -5,10 +5,18 @@ import Img from "gatsby-image"
 
 const Row = styled.div`
   display: flex;
+  flex-direction: column;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
 `
 
 const Col = styled.div`
   text-align: center;
+  @media screen and (min-width: 768px) {
+    flex: 1;
+  }
 `
 
 export default () => {
@@ -16,15 +24,15 @@ export default () => {
     query {
       spdigital: file(relativePath: { eq: "sponsors/spdigital.png" }) {
         childImageSharp {
-          fixed(width: 500) {
-            ...GatsbyImageSharpFixed
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
       microsoft: file(relativePath: { eq: "sponsors/microsoft.png" }) {
         childImageSharp {
-          fixed(width: 500) {
-            ...GatsbyImageSharpFixed
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
@@ -42,11 +50,11 @@ export default () => {
             <Row>
               <Col>
                 <h3>Platinum Sponsor</h3>
-                <Img fixed={data.spdigital.childImageSharp.fixed} />
+                <Img fluid={data.spdigital.childImageSharp.fluid} />
               </Col>
               <Col>
                 <h3>Silver Sponsor</h3>
-                <Img fixed={data.microsoft.childImageSharp.fixed} />
+                <Img fluid={data.microsoft.childImageSharp.fluid} />
               </Col>
             </Row>
           </div>
