3925a8ea6897f4ef40d771e95748c251a42781ea
Sheldon Cheng <sheldonscyd@gmail.com>
2019-10-02 23:20:50 +0800

    Prevent Console printing blank line if val is empty

--- src/components/terminal.js
+++ src/components/terminal.js
@@ -179,7 +179,7 @@ export default () => {
       if (Array.isArray(val)) {
         newLog = newLog.concat(val)
       } else {
-        newLog.push(val)
+        val && newLog.push(val)
       }
       setLog(newLog)
       e.target.value = ""
