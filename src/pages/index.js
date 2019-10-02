1ec7ffceb271e78b89f059f9e61a5abe77171761
Sheldon Cheng <sheldonscyd@gmail.com>
2019-10-02 23:40:01 +0800

    Fix build errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { graphql } from "gatsby"
 
 // components
 import Layout from "../components/layout"
--- src/sections/3.schedule.js
+++ src/sections/3.schedule.js
@@ -1,5 +1,4 @@
 import React from "react"
-import styled from "styled-components"
 import { ScheduleTime, ScheduleItem } from "../components/Schedule"
 
 export default () => (
