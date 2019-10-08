4fbe841bc77bc3a84308b6c12898a4f0b25cdc4e
Sheldon Cheng <sheldonscyd@gmail.com>
2019-10-09 00:19:04 +0800

    Add partner logos

--- src/sections/12.partners.js
+++ src/sections/12.partners.js
@@ -1,6 +1,13 @@
 import React from "react"
+import { css } from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
 import { Row, Col } from "../components/Grid"
+import nushackers from "../../static/images/partners/nushackers.svg"
+import twelvegeeks from "../../static/images/partners/12geeks.png"
+import junction from "../../static/images/partners/junction-x-singapore.png"
+import engineerssg from "../../static/images/partners/engineerssg.svg"
+import womenwhocode from "../../static/images/partners/womenwhocode.png"
+import osilayer8 from "../../static/images/partners/osi-layer8.svg"
 
 export default () => (
   <section className="section-partners" id="partners">
@@ -10,15 +17,40 @@ export default () => (
           <h2>Partners</h2>
         </div>
         <div className="right">
-          <section className="partners">
+          <section
+            className="partners"
+            css={`
+              ${Col} {
+                padding: 2rem;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+              }
+              img {
+                width: 100%;
+              }
+            `}
+          >
             <Row>
               <Col>
-                par
-                {/* <Img fluid={data.spdigital.childImageSharp.fluid} /> */}
+                <img alt="NUS Hackers" src={nushackers} />
               </Col>
               <Col>
-                pat
-                {/* <Img fluid={data.microsoft.childImageSharp.fluid} /> */}
+                <img alt="12 Geeks" src={twelvegeeks} />
+              </Col>
+              <Col>
+                <img alt="Junction X Singapore" src={junction} />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <img alt="Engineers.g" src={engineerssg} />
+              </Col>
+              <Col>
+                <img alt="Women Who Code" src={womenwhocode} />
+              </Col>
+              <Col>
+                <img alt="OSI Layer 8" src={osilayer8} />
               </Col>
             </Row>
           </section>
