f7a1e5662be31a40f17ce8c328395199877080eb
Terence Lim <terence.lim@salesforce.com>
2021-10-25 00:06:21 +0800

    feat: added committee members to data folder and iterate view

--- src/sections/13.committee.js
+++ src/sections/13.committee.js
@@ -1,15 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 
-import TerenceImage from "../images/committee/featured/terence.png"
-import AlanImage from "../images/committee/featured/alan.png"
-import AmbroseImage from "../images/committee/featured/ambrose.png"
-import HongKhengImage from "../images/committee/featured/hongkheng.png"
-import LutherImage from "../images/committee/featured/luther.png"
-import VinaImage from "../images/committee/featured/vina.png"
-import PerwylImage from "../images/committee/featured/perwyl.png"
-import StephImage from "../images/committee/featured/steph.png"
-import FazliImage from "../images/committee/featured/fazli.png"
+import CommitteeList from "../data/committee2021"
 
 const Grid = styled.div`
   display: grid;
@@ -29,59 +21,15 @@ export default () => {
 
           <div className="right">
             <Grid>
-              <div style={styles.imageContainer}>
-                <img src={PerwylImage} style={styles.imageStyle} />
-                <br />
-                Perwyl Liu
-              </div>
-
-              <div style={styles.imageContainer}>
-                <img src={LutherImage} style={styles.imageStyle} />
-                <br />
-                Luther Goh
-              </div>
-
-              <div style={styles.imageContainer}>
-                <img src={TerenceImage} style={styles.imageStyle} />
-                <br />
-                Terence Lim
-              </div>
-
-              <div style={styles.imageContainer}>
-                <img src={AlanImage} style={styles.imageStyle} />
-                <br />
-                Alan Swan
-              </div>
-
-              <div style={styles.imageContainer}>
-                <img src={HongKhengImage} style={styles.imageStyle} />
-                <br />
-                Hong Kheng Yap
-              </div>
-
-              <div style={styles.imageContainer}>
-                <img src={AmbroseImage} style={styles.imageStyle} />
-                <br />
-                Ambrose Chua
-              </div>
-
-              <div style={styles.imageContainer}>
-                <img src={VinaImage} style={styles.imageStyle} />
-                <br />
-                Vina Melody
-              </div>
-
-              <div style={styles.imageContainer}>
-                <img src={StephImage} style={styles.imageStyle} />
-                <br />
-                Stefanie Seah
-              </div>
-
-              <div style={styles.imageContainer}>
-                <img src={FazliImage} style={styles.imageStyle} />
-                <br />
-                Fazli Mansor
-              </div>
+              {CommitteeList.map((member) => {
+                return (
+                  <div style={styles.imageContainer}>
+                    <img src={member.imgsrc} style={styles.imageStyle} />
+                    <br />
+                    {member.name}
+                  </div>
+                )
+              })}
             </Grid>
           </div>
         </div>
