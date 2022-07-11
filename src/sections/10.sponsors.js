07fb4d4cf3d89ac7eebf4473dc0bd1686a291891
Eliot Lim <eliot.lim@gmail.com>
2022-07-11 17:28:29 +0800

    Update dependencies and eliminate critical vulns
    + Gatsby updated to 4.18.0
    + React updated to 18.2.0
    + gatsby-image updated to gatsby-plugin-image@2.18.0

--- src/sections/10.sponsors.js
+++ src/sections/10.sponsors.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import Img from "gatsby-plugin-image"
 import { Row, Col } from "../components/Grid"
 
 export default () => {
