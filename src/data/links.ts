824bc4c5058b6bcdb8e994ceb8d41f65f94762b6
Ambrose Chua <ambrose@hey.com>
2023-07-01 18:05:51 +0800

    Fix tweets, minor URL updates

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,6 @@
 export const volunteerLink = 'https://bit.ly/geekcampsg-volunteer-2022';
-export const discordLink = 'https://bit.ly/geekcampsg-discord';
+export const discordLink = '/discord';
+export const discordInviteUrl = 'https://discord.gg/26ydZDKhVp';
 export const ticketLink = 'https://bit.ly/geekcampsg-barcampsg-2022';
 export const venueLink = 'https://goo.gl/maps/2TyAFjJJQk1YrSYT8';
 export const venueName = 'SP Digital, 180 Clemenceau Ave, Singapore 239922';
