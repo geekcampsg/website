2884c84c543b30dcc7efcf3ff02f900783751830
Ambrose Chua <ambrose@hey.com>
2023-08-19 16:59:39 +0800

    feat: Update info about CFP

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,6 +1,6 @@
-export const volunteerLink = 'https://bit.ly/geekcampsg-volunteer-2022';
+export const volunteerLink = 'https://forms.gle/DeszWwSdEY423dj89';
 export const discordLink = '/discord';
 export const discordInviteUrl = 'https://discord.gg/26ydZDKhVp';
-export const ticketLink = 'https://bit.ly/geekcampsg-barcampsg-2022';
-export const venueLink = 'https://goo.gl/maps/2TyAFjJJQk1YrSYT8';
-export const venueName = 'SP Digital, 180 Clemenceau Ave, Singapore 239922';
+export const ticketLink = '';
+export const venueLink = 'https://goo.gl/maps/zLEay8AhTg32rYq67';
+export const venueName = 'WeWork 21 Collyer Quay';
