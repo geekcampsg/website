e7f951ddfc254a9e10e3e83cb635fd2436230c07
abc <abc@example.com>
2024-09-09 07:02:22 +0800

    fix: uncomment links in links.ts to fix deploy error

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,7 +1,7 @@
 export const volunteerLink = 'https://forms.gle/DeszWwSdEY423dj89';
 export const discordLink = '/discord';
 export const discordInviteUrl = 'https://discord.gg/26ydZDKhVp';
-/* export const ticketLink =
+export const ticketLink =
   '';
-export const venueLink = ''; */
+export const venueLink = '';
 export const venueName = 'TBC';
