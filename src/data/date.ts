375008a2e4b59357b3b18e8ffff11738ffe9ef73
Sam Tan <54680156+MisterDoobDoob@users.noreply.github.com>
2025-11-02 08:30:31 +0000

    add 2025 info

--- src/data/date.ts
+++ src/data/date.ts
@@ -1,6 +1,6 @@
-export const year = 2024;
+export const year = 2025;
 // Used by terminal for the date command
-export const date = new Date('2024-12-07T10:00:00+08:00');
+export const date = new Date('2025-12-13T10:00:00+08:00');
 // Used by the hero
-export const startDate = new Date('2024-12-07T10:00:00+08:00');
-export const endDate = new Date('2024-12-07T18:00:00+08:00');
+export const startDate = new Date('2025-12-13T10:00:00+08:00');
+export const endDate = new Date('2025-12-13T18:00:00+08:00');
